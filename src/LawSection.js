1f3ec2c3e70c76cf381cd750611c0a02368a349d
DIANA JURGIELEWICZ <DMJURG@GMAIL.com>
2018-07-30 21:49:14 -0400

    search functionality

--- src/LawSection.js
+++ src/LawSection.js
@@ -1,21 +1,68 @@
 import React from 'react';
 
 class LawSection extends React.Component {
-
   constructor(props) {
     super(props);
     this.state = {
       bodyVisible: false
     }
   }
+
+  shouldComponentUpdate(nextProps, nextState) {
+
+    const differentVisibleState = this.state.bodyVisible !== nextState.bodyVisible;
+    const differentText = this.props.bodyText !== nextProps.bodyText;
+    const differentSearchText = this.props.searchText !== nextProps.searchText;
+
+    if (differentSearchText && nextProps.searchText.length === 0) {
+      nextState.bodyVisible = false;
+    }
+    
+    const differentRegisterMatch = nextProps.registerMatch !== this.props.registerMatch;
+    return differentVisibleState || differentText || differentSearchText || differentRegisterMatch;
+   }
+
   render() {
+    const {bodyText, searchText} = this.props;
+
+    // don't search for short bits of text
+    if (!searchText || searchText.length < 3) {
+      return this.renderSection(bodyText);
+    }
+
+    const splitText = bodyText.split(searchText);
+    const els = [];
+
+    splitText.forEach((text, index) => {
+      els.push(<span key={`text-${index}`}>{text}</span>);
+      if (index === splitText.length - 1) {
+        return;
+      }
+
+      // there is a match!
+      this.setState({bodyVisible: true});
+
+      const match = <span className="highlight"
+                          key={`text-match-${index}`}
+                          ref={matchEl => this.props.registerMatch && this.props.registerMatch(matchEl)}>
+                      {searchText}
+                    </span>;
+      els.push(match);
+    })
+
+    return this.renderSection(els);
+  }
+
+  renderSection(text) {
     return (
       <section>
-        <h3 className="law-section-heading" onClick={() => {this.setState({bodyVisible: !this.state.bodyVisible})}}>
+        <h3 className="law-section-heading" onClick={() => {
+          this.setState({bodyVisible: !this.state.bodyVisible})}
+        }>
           {this.props.headingText}
         </h3>
         <div className="law-section-body" style={{display: this.state.bodyVisible ? 'block' : 'none'}}>
-          {this.props.bodyText}
+          {text}
         </div>
       </section>
     );
--- src/VNCA.js
+++ src/VNCA.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import LawSection from './LawSection';
-import Search from './Search';
 import {vncaText1} from './vncaText1';
 import {vncaText2} from './vncaText2';
 import {vncaText3} from './vncaText3';
@@ -16,50 +15,158 @@ import {vncaText11} from './vncaText11';
 import {vncaText111} from './vncaText111';
 
 class VNCA extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchText: "",
+      matches: [],
+      currentMatch: 0,
+    };
+
+    this.registerMatch = this.registerMatch.bind(this);
+  }
+
+  updateSearchText = (event) => {
+    this.setState({
+      searchText: event.target.value,
+      matches: [],
+      currentMatch: 0,
+    });
+  }
+
+  registerMatch(match) {
+    if (match) {
+      this.state.matches.push(match);
+    }
+
+    this.setState({
+      matches: this.state.matches,
+      currentMatch: 0
+    })
+  }
+
+  nextMatch = () => {
+    const {matches, currentMatch} = this.state;
+    const nextMatch = (currentMatch % matches.length) + 1;
+    this.setState({
+      currentMatch: nextMatch,
+    })
+    return nextMatch;
+  }
+
+  scrollToNextMatch = () => {
+    const {matches, currentMatch} = this.state;
+
+    if(matches.length === 0) {
+      return;
+    }
+
+    const nextMatch = this.nextMatch();
+    matches[nextMatch - 1].scrollIntoView();
+  }
+
   render() {
+    const {searchText} = this.state;
+
     return (
-      <div class="vnca-text">
+      <div className="vnca-text">
         <h1>Virginia Nonstock Corporation Act</h1>
-        <Search />
+        <div className="search-form">
+          <input value={searchText} onChange={this.updateSearchText} />
+          <button onClick={this.scrollToNextMatch}>Next</button>
+          {this.state.currentMatch} of {this.state.matches.length}
+        </div>
         <h2>Article 1. General Provisions</h2>
-        {vncaText1.map(section => <LawSection headingText={section.title}
-                                              bodyText={section.text} />)}
+        {vncaText1.map((section, index) => <LawSection headingText={section.title}
+                                              bodyText={section.text}
+                                              searchText={searchText}
+                                              registerMatch={this.registerMatch}
+                                              key={`section-${index}`}
+                                            />)}
         <h2>Article 2. Fees</h2>
-        {vncaText2.map(section => <LawSection headingText={section.title}
-                                              bodyText={section.text} />)}
+        {vncaText2.map((section, index) => <LawSection headingText={section.title}
+                                              bodyText={section.text}
+                                              searchText={searchText}
+                                              registerMatch={this.registerMatch}
+                                              key={`section-${index}`}
+                                            />)}
         <h2>Article 3. Formation of Corporations</h2>
-        {vncaText3.map(section => <LawSection headingText={section.title}
-                                              bodyText={section.text} />)}
+        {vncaText3.map((section, index) => <LawSection headingText={section.title}
+                                              bodyText={section.text}
+                                              searchText={searchText}
+                                              registerMatch={this.registerMatch}
+                                              key={`section-${index}`}
+                                            />)}
         <h2>Article 4. Purposes and Powers</h2>
-        {vncaText4.map(section => <LawSection headingText={section.title}
-                                              bodyText={section.text} />)}
+        {vncaText4.map((section, index) => <LawSection headingText={section.title}
+                                              bodyText={section.text}
+                                              searchText={searchText}
+                                              registerMatch={this.registerMatch}
+                                              key={`section-${index}`}
+                                            />)}
         <h2>Article 5. Name</h2>
-        {vncaText5.map(section => <LawSection headingText={section.title}
-                                              bodyText={section.text} />)}
+        {vncaText5.map((section, index) => <LawSection headingText={section.title}
+                                              bodyText={section.text}
+                                              searchText={searchText}
+                                              registerMatch={this.registerMatch}
+                                              key={`section-${index}`}
+                                            />)}
         <h2>Article 6. Office and Agent</h2>
-        {vncaText6.map(section => <LawSection headingText={section.title}
-                                              bodyText={section.text} />)}
+        {vncaText6.map((section, index) => <LawSection headingText={section.title}
+                                              bodyText={section.text}
+                                              searchText={searchText}
+                                              registerMatch={this.registerMatch}
+                                              key={`section-${index}`}
+                                            />)}
         <h2>Article 7. Members and Meetings</h2>
-        {vncaText7.map(section => <LawSection headingText={section.title}
-                                              bodyText={section.text} />)}
+        {vncaText7.map((section, index) => <LawSection headingText={section.title}
+                                              bodyText={section.text}
+                                              searchText={searchText}
+                                              registerMatch={this.registerMatch}
+                                              key={`section-${index}`}
+                                            />)}
         <h2>Article 7.1. Member or Director Agreements</h2>
-        {vncaText71.map(section => <LawSection headingText={section.title}
-                                               bodyText={section.text} />)}
+        {vncaText71.map((section, index) => <LawSection headingText={section.title}
+                                               bodyText={section.text}
+                                               searchText={searchText}
+                                               registerMatch={this.registerMatch}
+                                              key={`section-${index}`}
+                                             />)}
         <h2>Article 8. Directors and Officers</h2>
-        {vncaText8.map(section => <LawSection headingText={section.title}
-                                               bodyText={section.text} />)}
+        {vncaText8.map((section, index) => <LawSection headingText={section.title}
+                                               bodyText={section.text}
+                                               searchText={searchText}
+                                               registerMatch={this.registerMatch}
+                                              key={`section-${index}`}
+                                             />)}
         <h2>Article 9. Indemnification</h2>
-        {vncaText9.map(section => <LawSection headingText={section.title}
-                                              bodyText={section.text} />)}
+        {vncaText9.map((section, index) => <LawSection headingText={section.title}
+                                              bodyText={section.text}
+                                              searchText={searchText}
+                                              registerMatch={this.registerMatch}
+                                              key={`section-${index}`}
+                                            />)}
         <h2>Article 10. Amendment of Articles of Incorporation and Bylaws</h2>
-        {vncaText10.map(section => <LawSection headingText={section.title}
-                                              bodyText={section.text} />)}
+        {vncaText10.map((section, index) => <LawSection headingText={section.title}
+                                              bodyText={section.text}
+                                              searchText={searchText}
+                                              registerMatch={this.registerMatch}
+                                              key={`section-${index}`}
+                                            />)}
         <h2>Article 11. Merger</h2>
-        {vncaText11.map(section => <LawSection headingText={section.title}
-                                              bodyText={section.text} />)}
+        {vncaText11.map((section, index) => <LawSection headingText={section.title}
+                                              bodyText={section.text}
+                                              searchText={searchText}
+                                              registerMatch={this.registerMatch}
+                                              key={`section-${index}`}
+                                            />)}
         <h2>Article 11.1. Domestication</h2>
-        {vncaText111.map(section => <LawSection headingText={section.title}
-                                              bodyText={section.text} />)}
+        {vncaText111.map((section, index) => <LawSection headingText={section.title}
+                                              bodyText={section.text}
+                                              searchText={searchText}
+                                              registerMatch={this.registerMatch}
+                                              key={`section-${index}`}
+                                            />)}
       </div>
     );
   }
